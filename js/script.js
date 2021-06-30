1d50afa2c306d6320dc28719689227d59779e625
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-06-30 20:02:43 +0300

    adds a navbar and adds the event.prevent default in js

--- js/script.js
+++ js/script.js
@@ -6,20 +6,29 @@ var btnLogin = document.getElementById("login");
 var btnregister = document.getElementById("register");
 var btnreset = document.getElementById("reset");
 
-btnLogin.onclick = function(){
-    login.classList.add("active");
-    reset.classList.add("inActive");
-    register.classList.add("inActive");
+
+btnLogin.onclick = function(event){  
+    event.preventDefault();   
+     login.classList.add("active");
+     reset.classList.add("inActive");
+     register.classList.add("inActive");
     
 }
-btnregister.onclick = function(){
-   login.classList.remove("active");
-   reset.classList.add("inActive");
-   register.classList.remove("inActive");
-   }
-btnreset.onclick = function(){   
-   reset.classList.add("active"); 
-   login.classList.add("inactive");   
-   register.classList.add("inActive");   
+
+
+btnregister.onclick = function(event){ 
+    event.preventDefault();  
+    login.classList.remove("active");
+    reset.classList.add("inActive");
+    register.classList.remove("inActive");
+ }
+
+
+btnreset.onclick = function(event){ 
+    event.preventDefault();    
+    login.classList.add("inactive");   
+    register.classList.add("inActive"); 
+    reset.classList.add("active");   
 }
+
 
