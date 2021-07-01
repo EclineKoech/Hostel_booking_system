400aa9dd28f7b6a73dc68eb5eda469e2549625a3
Kinya <kinyajoy02@gmail.com>
2021-07-01 11:38:26 +0300

    add css html and js file

---
+++ hostel.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {  
+  $("div#image").click(function(event) {      
+      event.preventDefault();      
+  $("div#image").toggle();     
+  $("p#MADARAKA").toggle(); 
+  $("div#image").hide();
+  });
+  $("p#MADARAKA").click(function(event) {  
+   event.preventDefault();       
+   $("p#MADARAKA").toggle();      
+   $("div#image").toggle();      
+   $("div#image").show();
+    });});
+  $(document).ready(function () {  
+    $("div#image").click(function(event) {      
+        event.preventDefault();      
+    $("div#image").toggle();     
+    $("p#UHURU").toggle(); 
+    $("div#image").hide();
+    });
+    $("p#UHURU").click(function(event) {  
+     event.preventDefault();       
+     $("p#UHURU").toggle();      
+     $("div#image").toggle();      
+     $("div#image").show();
+    });});
+
+  
+$(document).ready(function () {  
+$("div#image").click(function(event) {      
+event.preventDefault();      
+$("div#image").toggle();     
+$("p#HURUMA").toggle(); 
+$("div#image").hide();
+});
+$("p#HURUMA").click(function(event) {  
+event.preventDefault();       
+$("p#HURUMA").toggle();      
+$("div#image").toggle();      
+$("div#image").show();
+});});
+
+
+$(document).ready(function () {  
+$("div#image").click(function(event) {      
+event.preventDefault();      
+$("div#image").toggle();     
+$("p#HARAMBEE").toggle(); 
+$("div#image").hide();
+});
+$("p#HARAMBEE").click(function(event) {  
+event.preventDefault();       
+$("p#HARAMBEE").toggle();      
+$("div#image").toggle();      
+$("div#image").show();
+});});
+$(document).ready(function () {  
+$("div#image").click(function(event) {      
+  event.preventDefault();      
+ $("div#image").toggle();     
+ $("p#UMOJA").toggle(); 
+ $("div#image").hide();
+ });
+$("p#UMOJA").click(function(event) {  
+event.preventDefault();       
+$("p#UMOJA").toggle();      
+ $("div#image").toggle();      
+ $("div#image").show();
+});});
+
+$(document).ready(function () {  
+$("div#image").click(function(event) {      
+ event.preventDefault();      
+  $("div#image").toggle();     
+$("p#IMARA").toggle(); 
+$("div#image").hide();
+ });
+$("p#IMARA").click(function(event) {  
+ event.preventDefault();       
+$("p#IMARA").toggle();      
+$("div#image").toggle();      
+$("div#image").show();
+});});
