7c1375ceea8c98d684d87bbbabff79e3eaff5191
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-07-01 04:56:28 +0300

    adds js to run on registration button click

--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
 //  transform the form on signup and login clicks
 var login = document.querySelector(".login");
 var register = document.querySelector(".register");
-var reset=document.querySelector(".reset");
+// var reset=document.querySelector(".reset");
 var btnLogin = document.getElementById("login");
 var btnregister = document.getElementById("register");
-var btnreset = document.getElementById("reset");
+// var btnreset = document.getElementById("reset");
 
+btnLogin.onclick = function () {
+    login.classList.add("active");
+    // reset.classList.add("inActive");
+    register.classList.add("inActive");
 
-btnLogin.onclick = function(event){  
-    event.preventDefault();   
-     login.classList.add("active");
-     reset.classList.add("inActive");
-     register.classList.add("inActive");
-    
 }
-
-
-btnregister.onclick = function(event){ 
-    event.preventDefault();  
+btnregister.onclick = function () {
     login.classList.remove("active");
-    reset.classList.add("inActive");
+    //    reset.classList.add("inActive");
     register.classList.remove("inActive");
- }
-
-
-btnreset.onclick = function(event){ 
-    event.preventDefault();    
-    login.classList.add("inactive");   
-    register.classList.add("inActive"); 
-    reset.classList.add("active");   
 }
+// btnreset.onclick = function(){   
+//    reset.classList.add("active"); 
+//    login.classList.add("inactive");   
+//    register.classList.add("inActive");   
+// }
+
+// The usernamer and passwordr are username and pass in the regitration section 
+
+$("#regbtn").click(function regLog (event) {
+    var usernamers = [];
+    var passwordrs = [];
+    var universitys = [];
+    var citys = [];
+    var usernamer = usernamers.push($("#usernamer").val());
+    var passwordr = passwordrs.push($("#passwordr").val());
+    var university = universitys.push($("#university").val());
+    var city = citys.push($("#city").val());
+    var usernames = usernamers.map();
+    var passwords = passwordrs.map();
+
+})
+    
+
 
 
