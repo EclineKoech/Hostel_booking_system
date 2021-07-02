d39076e738e88c3773ac11df8fdafa57894b5b04
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-07-02 08:44:59 +0300

    edited te js

--- js/script.js
+++ js/script.js
@@ -4,18 +4,37 @@ var register = document.querySelector(".register");
 // var reset=document.querySelector(".reset");
 var btnLogin = document.getElementById("login");
 var btnregister = document.getElementById("register");
+var regArray=[];
 // var btnreset = document.getElementById("reset");
 
-btnLogin.onclick = function () {
-    login.classList.add("active");
-    // reset.classList.add("inActive");
-    register.classList.add("inActive");
 
-}
 btnregister.onclick = function () {
     login.classList.remove("active");
     //    reset.classList.add("inActive");
     register.classList.remove("inActive");
+    var usernames=document.getElementById("usernamer").value;
+    var passwords=document.getElementById("passwordr").value;
+    var universitys=document.getElementById("university").value;
+    var citys=document.getElementById("city").value;
+var userobj={username:usernamer, password:passwordr,university:universityr,city:cityr};
+regArray.push(userobj);
+alert('Successfully registered!')
+
+}
+btnLogin.onclick = function () {
+    login.classList.add("active");
+    // reset.classList.add("inActive");
+    register.classList.add("inActive");
+    for (var i = 0; i < usernames.length; i++) {
+        if (username == usernames[i] && password == passwords[i]) {
+            // testing log. want to load a page
+            var url=$(this).attr('services.html');
+            break;
+        } else {
+            alert('Login failed check the credentials!')
+        }
+    }
+
 }
 // btnreset.onclick = function(){   
 //    reset.classList.add("active"); 
@@ -34,30 +53,31 @@ $("form").submit(function () {
 
 
 // The usernamer and passwordr are username and pass in the regitration section 
-var regArray=[];
-function onRegisterPressed(Event){
-    Event.preventDefault();
-    var usernames=document.getElementById("usernamer").value;
-    var passwords=document.getElementById("passwordr").value;
-    var universitys=document.getElementById("university").value;
-    var citys=document.getElementById("city").value;
-var userobj={username:usernamer, password:passwordr,university:universityr,city:cityr};
-regArray.push(userobj);
+// var regArray=[];
+// function onRegisterPressed(Event){
+//     Event.preventDefault();
+//     var usernames=document.getElementById("usernamer").value;
+//     var passwords=document.getElementById("passwordr").value;
+//     var universitys=document.getElementById("university").value;
+//     var citys=document.getElementById("city").value;
+// var userobj={username:usernamer, password:passwordr,university:universityr,city:cityr};
+// regArray.push(userobj);
+// alert('Successfully registered!')
 
 
-function onLoginPressed(){
-    for (var i = 0; i < usernames.length; i++) {
-                    if (username == usernames[i] && password == passwords[i]) {
-                        // testing log. want to load a page
-                        var url=$(this).attr('services.html');
-                        break;
-                    } else {
-                        alert('Login failed check the credentials!')
-                    }
+// function onLoginPressed(){
+//     for (var i = 0; i < usernames.length; i++) {
+//                     if (username == usernames[i] && password == passwords[i]) {
+//                         // testing log. want to load a page
+//                         var url=$(this).attr('services.html');
+//                         break;
+//                     } else {
+//                         alert('Login failed check the credentials!')
+//                     }
 
-}
-}
-};
+// }
+// }
+// ;
 // $(btnLogin).click(function (event) {
 //         event.preventDefault();
 //         for (var i = 0; i < usernames.length; i++) {
